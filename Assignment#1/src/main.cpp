a5734d668154e3fc951fac43b74958ef2b05f07e
Ryan Sun <rjsun@uci.edu>
2017-10-17 16:19:34 -0700

    first commit

--- Assignment#1/src/main.cpp
+++ Assignment#1/src/main.cpp
@@ -20,8 +20,9 @@ void printMacAddress()
 
     //Serial.println("print mac address");
     Serial.println(MAC_char);
-    
-
+}   
+void connectWifi()
+{
     WiFi.begin(ssid); //, password);
     while (WiFi.status() != WL_CONNECTED)
     {
@@ -35,17 +36,6 @@ void printMacAddress()
     
 }
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-    
-  Serial.begin(115200);
-  Serial.println("Program started");
-  printMacAddress();
-}
-
-
 void connectServer()
 {       
     if (WiFi.status() != WL_CONNECTED){
@@ -58,20 +48,22 @@ void connectServer()
         Serial.print("[HTTP] begin...\n");
         // configure traged server and url
         //http.begin("https://192.168.1.12/test.html", "7a 9c f4 db 40 d3 62 5a 6e 21 bc 5c cc 66 c8 3e a1 45 59 38"); //HTTPS
-        http.begin("http://13.57.112.90/"); //HTTP
+        http.begin("http://13.57.112.90/post.php/"); //HTTP
         
         //Serial.println(http.connected());
         
         digitalWrite(LED_BUILTIN, LOW);
-        Serial.print("[HTTP] GET...\n");
+        Serial.print("[HTTP] POST...\n");
         // start connection and send HTTP header
+
         //http.header()
-        int httpCode = http.GET();
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");        
+        int httpCode = http.POST("name=Hello from ESP8266!"); 
 
         // httpCode will be negative on error
         if(httpCode > 0) {
             // HTTP header has been send and Server response header has been handled
-            Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+            Serial.printf("[HTTP] POST... code: %d\n", httpCode);
 
             // file found at server
             if(httpCode == HTTP_CODE_OK) {
@@ -79,11 +71,21 @@ void connectServer()
                 Serial.println(payload);
             }
         } else {
-            Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+            Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
 
         http.end();
         digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void setup()
+{
+  // initialize LED digital pin as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+    
+  Serial.begin(115200);
+  Serial.println("Program started");
+  connectWifi();
 }
 
 void loop()
